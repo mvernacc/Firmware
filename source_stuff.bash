49e8ff9276e152e72217fbc79386fe28d1557630
Michael Kapteyn <mkapteyn@mit.edu>
2019-01-21 14:48:06 -0500

    Added aerofc.sdf

---
+++ source_stuff.bash
@@ -0,0 +1,4 @@
+source ~/vnav_ws/devel/setup.bash
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/posix_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
